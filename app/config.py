09b0993843d5ec4c91a5fe57596742a760ee01ae
Kian Bervoets <kianbervoets@gmail.com>
2025-05-15 22:43:03 +0200

    fliep floep

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = 'do-or-do-not-there-is-no-try'
 #    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql-rds-1.c1pqtqrgaiq5.us-east-1.rds.amazonaws.com'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:PostgreSQL-RDS-1@postgresql-rds-1.c1pqtqrgaiq5.us-east-1.rds.amazonaws.com:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
